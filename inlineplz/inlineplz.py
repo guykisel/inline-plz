757a773f2ef7a7e4af99aec8d78759f211220e0a
Guy Kisel <guy.kisel@gmail.com>
2016-01-09 14:13:01 -0800

    implement entrypoint

--- inlineplz/inlineplz.py
+++ inlineplz/inlineplz.py
@@ -8,19 +8,32 @@ import argparse
 import inlineplz.interfaces as interfaces
 import inlineplz.parsers as parsers
 
-def inline(filename, owner, repo, pr, user, token):
-    pass
+
+def inline(filename, parser, interface, owner, repo, pr, token, url):
+    with open(filename) as inputfile:
+        messages = parsers.PARSERS[parser].parse(inputfile.read())
+    my_interface = interfaces.INTERFACES[interface](owner, repo, pr, token, url)
+    my_interface.post_messages(messages)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--pr', type=int, required=True)
     parser.add_argument('--owner', type=str, required=True)
     parser.add_argument('--repo', type=str, required=True)
-    parser.add_argument('--token')
-    parser.add_argument('--user', type=str)
+    parser.add_argument('--token', type=str, require=True)
     parser.add_argument('--filename', type=str, required=True)
     parser.add_argument('--parser', type=str, required=True, choices=parsers.PARSERS)
     parser.add_argument('--interface', type=str, required=True, choices=interfaces.INTERFACES)
+    parser.add_argument('--url', type=str)
 
     args = parser.parse_args()
-    inline(args.filename, args.owner, args.repo, args.pr, args.user, args.token)
+    inline(
+        args.filename,
+        args.parser,
+        args.interface,
+        args.owner,
+        args.repo,
+        args.pr,
+        args.token,
+        args.url
+    )
