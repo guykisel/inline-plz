4bb8cafe0ecedadfe0381f30ed8f83acc38abc5f
Guy Kisel <guy.kisel@gmail.com>
2016-01-24 22:46:50 -0800

    fix test

--- tests/parsers/test_prospector.py
+++ tests/parsers/test_prospector.py
@@ -17,10 +17,14 @@ prospector_path = os.path.join(
 def test_prospector():
     with open(prospector_path) as inputfile:
         messages = prospector.ProspectorParser().parse(inputfile.read())
-        assert messages[0].content == '```\npylint: syntax-error / invalid syntax\n```'
+        assert messages[0].content == '`pylint: syntax-error / invalid syntax`'
         assert messages[0].line_number == 34
         assert messages[0].path == 'docs/conf.py'
-        assert messages[1].content == '```\npylint: unused-import / Unused Message imported from message\n```'
+        assert messages[1].content == '`pylint: unused-import / Unused Message imported from message`'
         assert messages[1].line_number == 4
         assert messages[1].path == 'inline-plz/parsers/base.py'
+        assert messages[9].content == ('```\npylint: misplaced-comparison-constant / Comparison '
+                                       'should be __name__ == \'__main__\' (col 3)\npylint: '
+                                       'pretend this is a real message\n```')
+        assert messages[9].line_number == 113
         assert len(messages) == 11
