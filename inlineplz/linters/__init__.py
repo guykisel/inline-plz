18384c8bac1af8b11bd162848c48fc9ae0e51b1c
Guy Kisel <guy.kisel@gmail.com>
2016-01-30 13:42:49 -0800

    add node linter configs

--- inlineplz/linters/__init__.py
+++ inlineplz/linters/__init__.py
@@ -6,14 +6,40 @@ from __future__ import absolute_import
 
 import os
 import subprocess
+import traceback
 
 from inlineplz import parsers
 
 LINTERS = {
     'prospector': {
+        'install': ['pip', 'install', 'prospector'],
         'run': ['prospector', '--zero-exit'],
-        'dotfile': '.prospector.yaml',
+        'dotfiles': ['.prospector.yaml'],
         'parser': parsers.ProspectorParser
+    },
+    'eslint': {
+        'install': ['npm', 'install', '-g', 'eslint'],
+        'run': ['eslint'],
+        'dotfiles': ['.eslintrc.yml'],
+        'parser': parsers.ESLintParser
+    },
+    'jshint': {
+        'install': ['npm', 'install', '-g', 'jshint'],
+        'run': ['jshint'],
+        'dotfiles': ['.jshintrc'],
+        'parser': parsers.JSHintParser
+    },
+    'jslint': {
+        'install': ['npm', 'install', '-g', 'jslint'],
+        'run': ['jslint'],
+        'dotfiles': ['.jslintrc'],
+        'parser': parsers.JSLintParser
+    },
+    'jscs': {
+        'install': ['npm', 'install', '-g', 'jscs'],
+        'run': ['jscs'],
+        'dotfiles': ['.jscsrc'],
+        'parser': parsers.JSCSParser
     }
 }
 
@@ -21,7 +47,11 @@ LINTERS = {
 def lint():
     messages = []
     for config in LINTERS.values():
-        if config.get('dotfile') in os.listdir(os.getcwd()):
-            output = subprocess.check_output(config.get('run')).decode('utf-8')
-            messages.extend(config.get('parser')().parse(output))
+        if any(dotfile in os.listdir(os.getcwd())
+               for dotfile in config.get('dotfiles')):
+            try:
+                output = subprocess.check_output(config.get('run')).decode('utf-8')
+                messages.extend(config.get('parser')().parse(output))
+            except subprocess.CalledProcessError:
+                traceback.print_exc()
     return messages
