e47ceb0dd0f767108de8e8a4656310fa14cf68f6
Guy Kisel <guy.kisel@gmail.com>
2016-01-30 19:15:22 -0800

    delete more jslint references

--- inlineplz/parsers/__init__.py
+++ inlineplz/parsers/__init__.py
@@ -6,13 +6,11 @@ from inlineplz.parsers.prospector import ProspectorParser
 from inlineplz.parsers.eslint import ESLintParser
 from inlineplz.parsers.jscs import JSCSParser
 from inlineplz.parsers.jshint import JSHintParser
-from inlineplz.parsers.jslint import JSLintParser
 
 
 PARSERS = {
     'prospector': ProspectorParser,
     'eslint': ESLintParser,
     'jshint': JSHintParser,
-    'jscs': JSCSParser,
-    'jslint': JSLintParser
+    'jscs': JSCSParser
 }
