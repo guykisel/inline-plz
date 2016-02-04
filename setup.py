55c3174a5a2aed3e5e6e20281fc02fb55f1d6f77
Guy Kisel <guy.kisel@gmail.com>
2016-02-03 18:07:44 -0800

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='inlineplz',
-    version='0.1.0',
+    version='0.1.1',
     description="Inline your lint messages",
     long_description=readme + '\n\n' + history,
     author="Guy Kisel",
