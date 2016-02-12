c14a9a94bfa7e815db296dc42510405830137ece
Guy Kisel <guy.kisel@gmail.com>
2016-02-11 16:19:04 -0800

    add autorun mode

--- inlineplz/linters/__init__.py
+++ inlineplz/linters/__init__.py
@@ -6,6 +6,7 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
+import glob
 import os
 import subprocess
 import traceback
@@ -13,12 +14,15 @@ import traceback
 from inlineplz import parsers
 from inlineplz import message
 
+
 LINTERS = {
     'prospector': {
         'install': ['pip', 'install', 'prospector'],
         'run': ['prospector', '--zero-exit', '-o', 'json'],
         'dotfiles': ['.prospector.yaml'],
-        'parser': parsers.ProspectorParser
+        'parser': parsers.ProspectorParser,
+        'glob': ['*.py', '**/*.py'],
+        'autorun': True
     },
     'eslint': {
         'install': ['npm', 'install'],
@@ -29,30 +33,43 @@ LINTERS = {
             '.eslintrc',
             'eslintrc.yml'
         ],
-        'parser': parsers.ESLintParser
+        'parser': parsers.ESLintParser,
+        'glob': ['*.js', '**/*.js'],
+        'autorun': True
     },
     'jshint': {
         'install': ['npm', 'install'],
         'run': [os.path.normpath('./node_modules/.bin/jshint'), '.', '--reporter', 'checkstyle'],
         'dotfiles': ['.jshintrc'],
-        'parser': parsers.JSHintParser
+        'parser': parsers.JSHintParser,
+        'glob': ['*.js', '**/*.js'],
+        'autorun': False
     },
     'jscs': {
         'install': ['npm', 'install'],
         'run': [os.path.normpath('./node_modules/.bin/jscs'), '.', '-r', 'json', '-m', '-1', '-v'],
         'dotfiles': ['.jscsrc', '.jscs.json'],
-        'parser': parsers.JSCSParser
+        'parser': parsers.JSCSParser,
+        'glob': ['*.js', '**/*.js'],
+        'autorun': True
     }
 }
 
 
-def lint(install=False):
+def should_autorun(config):
+    return config.get('autorun') and any(glob.glob(pattern) for pattern in config.get('glob'))
+
+
+def dotfiles_exist(config):
+    return any(dotfile in os.listdir(os.getcwd()) for dotfile in config.get('dotfiles'))
+
+
+def lint(install=False, autorun=False):
     messages = message.Messages()
     for linter, config in LINTERS.items():
-        if any(dotfile in os.listdir(os.getcwd())
-               for dotfile in config.get('dotfiles')):
+        if dotfiles_exist(config) or (autorun and should_autorun(config)):
             try:
-                if install and config.get('install'):
+                if (install or autorun) and config.get('install'):
                     subprocess.check_call(config.get('install'))
                 print(config.get('run'))
                 output = subprocess.check_output(config.get('run')).decode('utf-8')
