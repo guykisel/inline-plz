607061a1c7a9afccb877b32767719fd54da19fc8
Guy Kisel <guy.kisel@gmail.com>
2016-02-14 12:33:34 -0800

    add jsonlint support

---
+++ inlineplz/parsers/jsonlint.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from inlineplz.parsers.base import ParserBase
+
+
+class JSONLintParser(ParserBase):
+    """Parse json jsonlint output."""
+
+    def parse(self, lint_data):
+        messages = set()
+        for output in lint_data:
+            path = output.split(':')[0]
+            line = int(output.split(':')[1].split()[1].strip(', '))
+            msgbody = output
+            messages.add((path, line, msgbody))
+        return messages
