95b995f1b7423be77dc9dc658158ccfa92a852e7
Guy Kisel <guy.kisel@gmail.com>
2016-02-16 17:27:05 -0800

    crappy args hack

--- inlineplz/main.py
+++ inlineplz/main.py
@@ -49,7 +49,7 @@ def load_config(args):
         except yaml.parser.ParserError:
             pass
     args = update_from_config(args, config)
-    args.ignore_paths = args.getattr('ignore_paths') or ['node_modules']
+    args.ignore_paths = args.__dict__.get('ignore_paths') or ['node_modules']
     return args
 
 
