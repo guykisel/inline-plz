8328cf8ab2b4eb133917f6e8ee3edfe7593892da
Guy Kisel <guy.kisel@gmail.com>
2016-02-27 17:18:55 -0800

    add dotfiles_exist tests

--- tests/test_linter_utils.py
+++ tests/test_linter_utils.py
@@ -30,3 +30,29 @@ def test_run_config():
         'testdata', 'linter_configs'
     )
     assert linters.run_config(test_config, test_config_path) == ['rundefault']
+
+
+def test_dotfiles_dont_exist():
+    test_config = {
+        'run': ['run'],
+        'rundefault': ['rundefault'],
+        'dotfiles': ['.dotfile_doesnt_exist']
+    }
+    test_config_path = os.path.join(
+        os.getcwd(), 'tests',
+        'testdata', 'linter_configs'
+    )
+    assert not linters.dotfiles_exist(test_config, test_config_path)
+
+
+def test_dotfiles_exist():
+    test_config = {
+        'run': ['run'],
+        'rundefault': ['rundefault'],
+        'dotfiles': ['.dotfile']
+    }
+    test_config_path = os.path.join(
+        os.getcwd(), 'tests',
+        'testdata', 'linter_configs'
+    )
+    assert linters.dotfiles_exist(test_config, test_config_path)
