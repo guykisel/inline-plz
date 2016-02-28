434053fe50d3b6dd7fb984ebc90006accf19e735
Guy Kisel <guy.kisel@gmail.com>
2016-02-27 18:12:47 -0800

    add stylint support

---
+++ inlineplz/parsers/stylint.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from inlineplz.parsers.base import ParserBase
+
+
+class StylintParser(ParserBase):
+    """Parse stylint output."""
+
+    def parse(self, lint_data):
+        messages = set()
+        current_path = None
+        current_line = None
+        current_message = None
+        for line in lint_data.split('\n'):
+            if line.startswith('File:'):
+                current_path = line.split('File:')[-1].strip()
+            elif line.startswith('Line:'):
+                current_line = int(line.split(':')[1])
+            elif line:
+                current_message = line.strip()
+            if all([current_line, current_path, current_message]):
+                messages.add((current_path, current_line, current_message))
+                current_path = None
+                current_line = None
+                current_message = None
+        return messages
