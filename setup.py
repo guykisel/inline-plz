3383ae72f368fb15fe1414c9ce6fde5772c32c24
Raphael Castaneda <racastaneda@teslamotors.com>
2016-03-16 15:31:50 -0700

    stashy version 0.3.0 -> 0.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('HISTORY.rst') as history_file:
 requirements = [
     'unidiff',
     'github3.py',
-    'stashy>=0.3.0',
+    'stashy>=0.3',
     'xmltodict',
     'pyyaml',
     'scandir'
@@ -36,7 +36,7 @@ setup(
     include_package_data=True,
     install_requires=requirements,
     dependency_links=[  # TODO: Remove dependency_links once stashy is updated on PyPI
-        'git+https://github.com/RisingOak/stashy.git#egg=stashy-0.3.0'
+        'git+https://github.com/RisingOak/stashy.git#egg=stashy-0.3'
     ],
     license="ISCL",
     zip_safe=False,
