7fef1312e2709c2735859b2dcb064c96da3bdc5c
Guy Kisel <guy.kisel@gmail.com>
2016-04-12 16:11:31 -0700

    use unicode literals

--- inlineplz/linters/__init__.py
+++ inlineplz/linters/__init__.py
@@ -5,6 +5,7 @@
 
 from __future__ import absolute_import
 from __future__ import print_function
+from __future__ import unicode_literals
 
 import fnmatch
 from multiprocessing.pool import ThreadPool as Pool
