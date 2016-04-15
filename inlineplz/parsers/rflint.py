d5f76cdf3c39c63926ef26c8a45e5100c1b784e5
raphaelcastaneda <raphael.castaneda@gmail.com>
2016-04-15 16:27:25 -0700

    configure rflint to run per file (#129)
    * configure rflint to run per file
    
    fixes #121
    
    * remove directory from rflint run commands
    
    * update rflint parser to handle run_per_file input
    
    * use rflint parsed output for file path
    
    use what rflint said instead of trusting the input that came from the
    call string
    
    I also updated this to handle paths with spaces in them, which would
    previously break how this was being parsed
    
    * update rflint test to simlulate run_per_file input

--- inlineplz/parsers/rflint.py
+++ inlineplz/parsers/rflint.py
@@ -11,16 +11,17 @@ class RobotFrameworkLintParser(ParserBase):
     def parse(self, lint_data):
         messages = set()
         current_file = None
-        for line in lint_data.split('\n'):
-            try:
-                if line.startswith('+'):
-                    current_file = line.split(' ')[1].strip()
-                    continue
-                else:
-                    _, position, message = line.split(':')
-                    line_number, _ = position.split(',')
-                    messages.add((current_file, int(line_number), message.strip()))
-            except ValueError:
-                pass
+        for _, output in lint_data:
+            for line in output.split('\n'):
+                try:
+                    if line.startswith('+'):
+                        current_file = line[2:]
+                        continue
+                    else:
+                        _, position, message = line.split(':')
+                        line_number, _ = position.split(',')
+                        messages.add((current_file, int(line_number), message.strip()))
+                except ValueError:
+                    pass
 
         return messages
