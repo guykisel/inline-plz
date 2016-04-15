9a59b4c87e8208dee36ef16f41cf30012635d199
Raphael Castaneda <racastaneda@teslamotors.com>
2016-04-15 14:55:17 -0700

    update rflint test to simlulate run_per_file input

--- tests/parsers/test_rflint.py
+++ tests/parsers/test_rflint.py
@@ -17,7 +17,14 @@ rflint_path = os.path.join(
 
 def test_rflint():
     with codecs.open(rflint_path, encoding='utf-8', errors='replace') as inputfile:
-        messages = sorted(list(rflint.RobotFrameworkLintParser().parse(inputfile.read())))
-        assert messages[-1][2] == 'Too few steps (1) in keyword (TooFewKeywordSteps)'
-        assert messages[-1][1] == 30
-        assert messages[-1][0] == './Functional_Requirements/keywords.robot'
+        test_data = inputfile.readlines()
+        test_filename = ''
+        test_input = []
+        for line in test_data:
+            if line.startswith('+'):
+                test_filename = line.split(' ')[-1].strip()
+            test_input.append((test_filename, line))
+    messages = sorted(list(rflint.RobotFrameworkLintParser().parse(test_input)))
+    assert messages[-1][2] == 'Too few steps (1) in keyword (TooFewKeywordSteps)'
+    assert messages[-1][1] == 30
+    assert messages[-1][0] == './Functional_Requirements/keywords.robot'
