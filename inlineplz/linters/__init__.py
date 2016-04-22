8f3e57156687edc02fe25fe7da9500087cdc6c25
Guy Kisel <guy.kisel@gmail.com>
2016-04-21 17:01:45 -0700

    use time.time instead of time.clock

--- inlineplz/linters/__init__.py
+++ inlineplz/linters/__init__.py
@@ -343,7 +343,7 @@ def lint(install=False, autorun=False, ignore_paths=None, config_dir=None):
     for linter in linters_to_run(install, autorun, ignore_paths):
         print('Running linter: {0}'.format(linter))
         sys.stdout.flush()
-        start = time.clock()
+        start = time.time()
         output = ''
         config = LINTERS.get(linter)
         try:
@@ -358,9 +358,9 @@ def lint(install=False, autorun=False, ignore_paths=None, config_dir=None):
         except Exception:
             traceback.print_exc()
             print(str(output).encode('ascii', errors='replace'))
-        print('Installation and running of {0} took {1} seconds'.format(linter, int(time.clock() - start)))
+        print('Installation and running of {0} took {1} seconds'.format(linter, int(time.time() - start)))
         sys.stdout.flush()
-        start = time.clock()
+        start = time.time()
         try:
             if output:
                 linter_messages = config.get('parser')().parse(output)
@@ -372,5 +372,5 @@ def lint(install=False, autorun=False, ignore_paths=None, config_dir=None):
         except Exception:
             traceback.print_exc()
             print(str(output).encode('ascii', errors='replace'))
-        print('Parsing of {0} took {1} seconds'.format(linter, int(time.clock() - start)))
+        print('Parsing of {0} took {1} seconds'.format(linter, int(time.time() - start)))
     return messages.get_messages()
