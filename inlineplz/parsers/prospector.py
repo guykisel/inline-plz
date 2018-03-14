2b479e16d71581505155fbb7231a21cdb37935bf
Guy Kisel <gkisel@riotgames.com>
2018-03-14 13:20:19 -0700

    better dirtyjson error handling

--- inlineplz/parsers/prospector.py
+++ inlineplz/parsers/prospector.py
@@ -2,6 +2,8 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import traceback
+
 import dirtyjson as json
 
 from inlineplz.parsers.base import ParserBase
@@ -12,16 +14,20 @@ class ProspectorParser(ParserBase):
 
     def parse(self, lint_data):
         messages = set()
-        for msgdata in json.loads(lint_data).get('messages'):
-            try:
-                path = msgdata['location']['path']
-                line = msgdata['location']['line']
-                msgbody = '{0}: {1} ({2})'.format(
-                    msgdata['source'],
-                    msgdata['message'],
-                    msgdata['code']
-                )
-                messages.add((path, line, msgbody))
-            except (ValueError, KeyError):
-                print('Invalid message: {0}'.format(msgdata))
+        try:
+            for msgdata in json.loads(lint_data).get('messages'):
+                try:
+                    path = msgdata['location']['path']
+                    line = msgdata['location']['line']
+                    msgbody = '{0}: {1} ({2})'.format(
+                        msgdata['source'],
+                        msgdata['message'],
+                        msgdata['code']
+                    )
+                    messages.add((path, line, msgbody))
+                except (ValueError, KeyError):
+                    print('Invalid message: {0}'.format(msgdata))
+        except ValueError:
+            traceback.print_exc()
+            print(lint_data)
         return messages
