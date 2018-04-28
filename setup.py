919d6c35983758ab51104f3697a5eb8bad75ff4d
Guy Kisel <guykisel@users.noreply.github.com>
2018-04-27 17:19:58 -0700

    pin unidiff to 0.5.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'unidiff',
+    'unidiff==0.5.4',
     'github3.py',
     'xmltodict',
     'pyyaml',
