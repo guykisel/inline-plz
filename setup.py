71740b7eaf33c09ea9928ae27dc2e4a86475e194
Guy Kisel <guykisel@users.noreply.github.com>
2018-04-30 18:38:05 -0700

    don't strip diffs (#227)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'unidiff==0.5.4',
+    'unidiff',
     'github3.py',
     'xmltodict',
     'pyyaml',
