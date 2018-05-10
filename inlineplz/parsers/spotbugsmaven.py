59b15faf88f34f869315e1ad32b1adc8cf69d6f0
Guy Kisel <gkisel@riotgames.com>
2018-05-09 18:38:19 -0700

    fix path splitting

--- inlineplz/parsers/spotbugsmaven.py
+++ inlineplz/parsers/spotbugsmaven.py
@@ -20,7 +20,7 @@ class SpotbugsMavenParser(ParserBase):
                 if '[line' in line:
                     msgbody = line
                     line_number = int(line.split('[line ')[1].split(']')[0])
-                    path_parts = line.split('[')[1].split(']')[0].split('.')
+                    path_parts = line.split('[')[-2].split(']')[0].split('$')[0].split('.')
                     path_parts[-1] = path_parts[-1] + '.java'
                     path_base = [project, 'src', 'main', 'java']
                     path_base.extend(path_parts)
