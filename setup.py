52a3cd8568e4ed5a22260591c512a1d3c68eca1d
requires.io <support@requires.io>
2018-05-31 17:33:50 -0700

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='inlineplz',
-    version='0.35.0',
+    version='0.35.1',
     description="Inline your lint messages",
     long_description=readme + '\n\n' + history,
     author="Guy Kisel",
