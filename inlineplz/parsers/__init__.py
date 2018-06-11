e485a6383ceba01a59ec14dc05f6d23a99344b7a
Guy Kisel <guy.kisel@gmail.com>
2018-06-11 16:26:10 -0700

    fix missing import

--- inlineplz/parsers/__init__.py
+++ inlineplz/parsers/__init__.py
@@ -8,6 +8,7 @@ from inlineplz.parsers.ansiblelint import AnsibleLintParser
 from inlineplz.parsers.bandit import BanditParser
 from inlineplz.parsers.codenarc import CodenarcParser
 from inlineplz.parsers.dockerfilelint import DockerfileLintParser
+from inlineplz.parsers.eclint import ECLintParser
 from inlineplz.parsers.eslint import ESLintParser
 from inlineplz.parsers.gherkinlint import GherkinLintParser
 from inlineplz.parsers.gometalinter import GometalinterParser
