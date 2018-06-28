04387b6d0b21efb5dcb4d2b05d374975ffba4a88
Guy Kisel <gkisel@riotgames.com>
2018-06-28 11:28:37 -0700

    Bump version: 0.37.9 → 0.37.10

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='inlineplz',
-    version='0.37.9',
+    version='0.37.10',
     description="Inline your lint messages",
     long_description=readme + '\n\n' + history,
     author="Guy Kisel",
