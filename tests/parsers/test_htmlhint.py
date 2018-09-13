ab060bdbccec42944277ab8ad90ab1e408cda877
Arvind Paul <arvind.paul@riotgames.com>
2018-09-12 19:24:04 -0700

    htmlhint linter

---
+++ tests/parsers/test_htmlhint.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import codecs
+import os.path
+
+import inlineplz.parsers.htmlhint as htmlhint
+
+htmlhint_path = os.path.join("tests", "testdata", "parsers", "htmlhint.txt")
+
+
+def test_htmlhint():
+    with codecs.open(htmlhint_path, encoding="utf-8", errors="replace") as inputfile:
+        messages = sorted(list(htmlhint.HTMLHintParser().parse(inputfile.read())))
+        assert messages[0][0] == "./data/executable.html"
+        assert messages[0][1] == 8
+        assert (
+            messages[0][2] == "Duplicate of attribute name [ bad ] was found."
+        )
