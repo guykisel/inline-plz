508c688d261c40efabebd5c410fb4ba9d719b04b
Kyle Burton <kyle.burton@gmail.com>
2018-09-14 11:55:49 -0700

    335 todolint (#364)

---
+++ inlineplz/linters/todolint.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import re
+from inlineplz.decorators import linter
+from inlineplz.parsers.base import ParserBase
+
+
+@linter(
+    {
+        "name": "todolint",
+        "url": "https://bitbucket.org/atlassian/todolint",
+        "language": "all",
+        "patterns": ["*.*"],
+        "install": [["go", "get", "-u", "bitbucket.org/atlassian/todolint"]],
+        "help": ["todolint", "--help"],
+        "run": ["todolint"],
+        "rundefault": ["todolint"],
+        "dotfiles": [],
+        "autorun": True,
+        "run_per_file": False,
+    }
+)
+class TodoLintParser(ParserBase):
+    """Parse todolint output."""
+
+    def parse(self, lint_data):
+        messages = set()
+        lines = re.compile(r"\r?\n").split(lint_data)
+        line_pat = re.compile(r"^(.+?) \((\d+)\): (.+)$")
+
+        for line in lines:
+            match = line_pat.search(line)
+            if not match:
+                continue
+            messages.add(
+                (
+                    match.group(1).strip(),
+                    int(match.group(2).strip()),
+                    match.group(3).strip(),
+                )
+            )
+
+        return messages
