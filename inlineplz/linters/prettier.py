d91e8f3fd6fc2d1c2d071188a13693d77b08d447
Guy Kisel <guykisel@users.noreply.github.com>
2018-11-07 12:04:28 -0800

    Prettier (#410)
    fixes #407

---
+++ inlineplz/linters/prettier.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import os.path
+
+from ..decorators import linter
+from ..parsers.base import ParserBase
+
+
+@linter(
+    name="prettier",
+    install=[["npm", "install", "prettier"]],
+    help_cmd=[os.path.normpath("./node_modules/.bin/prettier"), "-h"],
+    run=[os.path.normpath("./node_modules/.bin/prettier"), "--write"],
+    rundefault=[
+        os.path.normpath("./node_modules/.bin/prettier"), "--write"
+    ],
+    dotfiles=[],
+    language="all",
+    autorun=False,
+    run_per_file=True,
+    autofix=True,
+)
+class PrettierParser(ParserBase):
+    """Prettier isn't actually a linter, so no-op."""
+
+    def parse(self, lint_data):
+        return []
