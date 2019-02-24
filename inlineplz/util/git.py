fa14170d3b227b8a4457263ac959fe035b227efb
Guy Kisel <guy.kisel@gmail.com>
2019-02-24 13:03:57 -0800

    wip clone dotfile repos

--- inlineplz/util/git.py
+++ inlineplz/util/git.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import os
 import subprocess
+import tempfile
 
 
 def current_sha():
@@ -100,3 +102,42 @@ def command(*args):
     return (
         subprocess.check_output(git_command).strip().decode("utf-8", errors="replace")
     )
+
+
+def clone(url, dir=None, token=None, ref=None):
+    if not dir:
+        dir = os.getcwd()
+    if token:
+        # https://github.com/blog/1270-easier-builds-and-deployments-using-git-over-https-and-oauth
+        url = url.replace('https://', 'https://{}@'.format(token))
+    print('Cloning: {}'.format(url))
+    try:
+        os.makedirs(dir)
+    except OSError:
+        pass
+    try:
+        subprocess.check_call(
+            ['git', 'init'],
+            cwd=dir
+        )
+
+        pull_cmd = ['git', 'pull', url]
+        if ref:
+            pull_cmd.append(ref)
+        subprocess.check_call(
+            pull_cmd,
+            cwd=dir
+        )
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
+def clone_dotfiles(url, org, token=None):
+    dotfile_dir = tempfile.mkdtemp()
+    for repo in ["dotfiles", ".dotfiles", ".github"]:
+        clone_url = '/'.join([url, org, repo]) + '.git'
+        print('Cloning: {}'.format(clone_url))
+        dotfile_path = os.path.join(dotfile_dir, repo)
+        if clone(clone_url, dotfile_path, token):
+            return dotfile_path
