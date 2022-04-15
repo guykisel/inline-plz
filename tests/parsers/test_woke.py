06617a1563349db9bc56b18781217ff14ccc6ad3
stickler-ci <support@stickler-ci.com>
2022-04-15 03:37:25 +0000

    Fixing style errors.

--- tests/parsers/test_woke.py
+++ tests/parsers/test_woke.py
@@ -9,26 +9,19 @@ woke_path = os.path.join("tests", "testdata", "parsers", "woke.txt")
 
 expected_messages = list()
 expected_messages.append(
-    ("sql/core/src/main/scala/org/apache/spark/sql/execution/joins/HashedRelation.scala"
-     , 406
-     , "`dummy` may be insensitive, use `placeholder`, `sample` instead (error)")
+    ("sql/core/src/main/scala/org/apache/spark/sql/execution/joins/HashedRelation.scala", 406, "`dummy` may be insensitive, use `placeholder`, `sample` instead (error)")
 )
 expected_messages.append(
-    ("sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/FileFormatWriter.scala"
-     , 208
-     , "`dummy` may be insensitive, use `placeholder`, `sample` instead (error)")
+    ("sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/FileFormatWriter.scala", 208, "`dummy` may be insensitive, use `placeholder`, `sample` instead (error)")
 )
 expected_messages.append(
-    ("docs/js/vendor/bootstrap.bundle.min.js.map"
-     , 1
-     , "`whiteList` may be insensitive, use `allowlist`, `inclusion list` instead (warning)")
+    ("docs/js/vendor/bootstrap.bundle.min.js.map", 1, "`whiteList` may be insensitive, use `allowlist`, `inclusion list` instead (warning)")
 )
 expected_messages.append(
-    ("sql/core/src/main/scala/org/apache/spark/sql/execution/adaptive/InsertAdaptiveSparkPlan.scala"
-     , 107
-     , "`sanity` may be insensitive, use `confidence`, `quick check`, `coherence check` instead (error)")
+    ("sql/core/src/main/scala/org/apache/spark/sql/execution/adaptive/InsertAdaptiveSparkPlan.scala", 107, "`sanity` may be insensitive, use `confidence`, `quick check`, `coherence check` instead (error)")
 )
 expected_messages = sorted(expected_messages)
+
 
 def test_woke():
     with codecs.open(woke_path, encoding="utf-8", errors="replace") as inputfile:
